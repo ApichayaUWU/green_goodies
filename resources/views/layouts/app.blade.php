7ec0e5c95a25fd84150c7064e13ea6b7cd24130b
FAH <apichaya.inkhiewsai@gmail.com>
2024-09-29 14:36:54 +0700

    add : footer at layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,12 +28,43 @@
                 </header>
             @endisset
 
+            <div class="min-h-screen">
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
-            
+            </div>
 
+            <!-- Footer -->
+            <div>
+            <footer class="bg-[#f7f0e8] w-full py-6 flex justify-around items-center">
+                <!-- Logo and slogan -->
+                <div class="flex flex-col items-center">
+
+                    <img src="{{ asset('storage/' . 'images/กล้วยหอม.jpg') }}" alt="Green Goodies Logo" class="h-16 mb-2">
+                    
+                    <h1 class="text-green-600 text-2xl font-bold">Green Goodies</h1>
+                    <p class="text-gray-600">Goodies Grown with Love</p>
+                </div>
+                
+                <!-- Contact info -->
+                <div class="flex flex-col items-center">
+                    <h2 class="text-gray-800 font-semibold">Contact Us</h2>
+                    <p class="text-gray-600">Tel: 099-999-9999</p>
+                    <p class="text-gray-600">Email: greengoodies@gmail.com</p>
+                </div>
+
+                <!-- Social Media Icons -->
+                <div class="flex flex-col items-center">
+                    <h2 class="text-gray-800 font-semibold">Social Media</h2>
+                    <div class="flex space-x-4 mt-2">
+                        <a href="#"><img src="{{ asset('storage/' . 'images/facebook.png') }}" alt="Facebook" class="h-6 w-6" /></a>
+                        <a href="#"><img src="{{ asset('storage/' . 'images/instagram.png') }}" alt="Instagram" class="h-6 w-6" /></a>
+                        <a href="#"><img src="{{ asset('storage/' . 'images/twitter.png') }}" alt="Twitter" class="h-6 w-6" /></a>
+                    </div>
+                </div>
+            </footer>
+            </div>
         </div>
     </body>
 </html>
