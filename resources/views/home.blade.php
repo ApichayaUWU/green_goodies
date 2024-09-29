0a636f2acf1bf2105e11bcb28d8923cd36a10fab
FAH <apichaya.inkhiewsai@gmail.com>
2024-09-29 13:35:58 +0700

    add : shop now lbutton link to our product page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Green Goodies') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <!-- ----- -->
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+
+            <a href="{{ route('products.index') }}" class="text-blue-600 hover:underline">
+                <x-primary-button>{{ __('Shop Now') }}</x-primary-button>
+            </a>
+           
+
             </div>
         </div>
     </div>
