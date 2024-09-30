d55da9c855efe715a8bf68dbf8899084900fa3c4
penpizza <penpicha.thongkham@gmail.com>
2024-09-30 14:25:51 +0700

    create welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-       
-<link href="https://fonts.bunny.net/css?family=manjari:700" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=manjari:700" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
