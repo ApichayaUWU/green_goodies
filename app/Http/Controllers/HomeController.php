56955a301689f0ba02d3bd40d3f57bbde2094158
Issawara <kmind46@gmail.com>
2024-10-02 18:49:09 +0700

    Sold out

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,8 @@ class HomeController extends Controller
     public function popular()
     {
         // Fetch products ordered by popularity (descending), limit to top 5 for example
+        // $popularProducts = Product::where('stock_quantity', '>', 0)->orderBy('popularity', 'desc')->limit(10)->get(); 
         $popularProducts = Product::orderBy('popularity', 'desc')->limit(10)->get(); 
-
         // Return the view with the products
         return view('home', [
             'popularProducts' => $popularProducts, // Pass the popular products to the view
