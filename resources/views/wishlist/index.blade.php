981a2be1899ac61e548df17f3b05a7c2286f403d
Issawara <kmind46@gmail.com>
2024-10-03 15:00:45 +0700

    Update frontend

--- resources/views/wishlist/index.blade.php
+++ resources/views/wishlist/index.blade.php
@@ -14,7 +14,14 @@
 </svg>
 <div class="p-3 text-gray-900">
             @if($wishlistItems->isEmpty())
-            <p class="no">You have not added any items to your wishlist at this time.</p>
+            <div class = "flex flex-row justify-center">
+                    <div><svg xmlns="http://www.w3.org/2000/svg" width="250" height="250" fill="#b8b8b8" class="bi bi-heartbreak-fill mt-14" viewBox="0 0 16 16">
+                        <path d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586M7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77"/>
+                    </svg></div>
+                    </div>
+                    <div class = "flex flex-row justify-center my-6"><div>
+                        <p class="p-2 text-2xl text-[#b8b8b8] ml-4">- You have not added any items to your wishlist at this time. -</p>
+                    </div></div>
             @else
             <div class="flex flex-row flex-wrap justify-center">
                 @foreach ($wishlistItems as $item)
