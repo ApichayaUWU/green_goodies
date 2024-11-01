8962af186a16a3f01f21b2ed041865d185721713
FAH <apichaya.inkhiewsai@gmail.com>
2024-11-01 12:31:49 +0700

    fix : positioning cross btn

--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -310,7 +310,7 @@
                     </button>
                 </div>
             </form>
-            <button class="absolute top-0 right-[420px] p-2" onclick="closeModal()">
+            <button class="absolute top-0 right-[370px] p-2" onclick="closeModal()">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
